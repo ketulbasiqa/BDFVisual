bc246ffdfcebc6d1ccac1b39640f7f417d6a79a2
ketul@basiqa.com <ketul@basiqa.com>
2016-09-09 05:14:49 -0400

    Version 1

--- inFolder/BDFSVisual/src/main/java/com/kp/BdfsVisualApplication.java
+++ inFolder/BDFSVisual/src/main/java/com/kp/BdfsVisualApplication.java
@@ -8,5 +8,6 @@ public class BdfsVisualApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdfsVisualApplication.class, args);
+		System.out.println("first git test");
 	}
 }
