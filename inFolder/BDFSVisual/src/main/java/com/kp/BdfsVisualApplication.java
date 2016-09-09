fee801a17ae26753a0beb79557803d6fa30c0c74
ketul@basiqa.com <ketul@basiqa.com>
2016-09-09 05:12:46 -0400

    First Commit

---
+++ inFolder/BDFSVisual/src/main/java/com/kp/BdfsVisualApplication.java
@@ -0,0 +1,12 @@
+package com.kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdfsVisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdfsVisualApplication.class, args);
+	}
+}
